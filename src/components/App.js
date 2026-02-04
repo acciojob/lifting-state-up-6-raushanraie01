f72d37f93c9cde7fa83df5bd5ea0a256813097b9
raushanraie01 <raushanraie01@gmail.com>
2026-02-05 02:16:35 +0530

    done5

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,22 @@ import "./../styles/App.css";
 import TodoList from "./TodoList";
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, data: "Learn React", isComplete: false },
-    { id: 2, data: "Build a React app", isComplete: false },
-    { id: 3, data: "Deploy the React app", isComplete: false },
+    { id: 1, data: "Learn React", completed: false },
+    { id: 2, data: "Build a React app", completed: false },
+    { id: 3, data: "Deploy the React app", completed: false },
   ]);
   console.group("todos", todos);
   function handleComplete(id) {
     setTodos((prev) =>
       prev.map((todo) => {
-        return todo.id == id ? { ...todo, isComplete: true } : todo;
+        return todo.id == id ? { ...todo, completed: true } : todo;
       }),
     );
   }
 
   return (
     <div>
+      <h1>Parent Component</h1>
       <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,7 @@ import React from "react";
 function TodoList({ todos, handleComplete }) {
   return (
     <ul>
+      <h3>Child Component</h3>
       {todos.map((todo, index) => (
         <li key={index}>
           {todo.data}
